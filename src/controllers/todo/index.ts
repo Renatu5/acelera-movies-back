68a2b709bfe7a07d66ab4953a78d2356ed713a1e
renatu5 <renatoosoouzaa26@gmail.com>
2022-08-03 15:04:57 -0300

    implemento do endpoint put

--- src/controllers/todo/index.ts
+++ src/controllers/todo/index.ts
@@ -48,11 +48,20 @@ export const postMovie = async (request, response) => {
 export const deleteMovie = async (request, response) => {
   try {
     const moviesRepository = getRepository(movie)
-    const { id } = request.params
+    const id = request.params
     const deleteMovie = await moviesRepository.delete(id)
 
     return response.status(200).json(deleteMovie)
   } catch (error) {
     return response.status(404).json(error)
   }
+}
+
+export const updateMovie = async (request, response) => {
+  const { id } = request.params
+  const body = request.body
+  const moviesRepository = getRepository(movie)
+  const updatedMovie = await moviesRepository.update(id, body)
+
+  return response.status(200).json(updatedMovie)
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import {
   getTODO,
   itsWorks,
   postMovie,
+  updateMovie,
 } from "@controllers/todo"
 
 export const defineRoutes = (app) => {
@@ -14,5 +15,6 @@ export const defineRoutes = (app) => {
   app.get("/movie_list", getAllMovies)
   app.get("/movie_list/:id", getMovieId)
   app.post("/movie_list", postMovie)
+  app.put("/movie_list/:id", updateMovie)
   app.delete("/movie_list/:id", deleteMovie)
 }
